dc7302cf138576dd66a4e5094580abe4118561b5
Yasmina Bocereg <yasminabocereg@Yasminas-MacBook-Pro.local>
2022-04-07 18:53:10 +0300

    [3]:user-can-see-board-with-squares

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      <div className="board"/>
+      <div className="board">
+        <div className="item"></div>
+      </div>
     </div>
   );
 }
