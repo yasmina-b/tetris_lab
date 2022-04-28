582ac79e55ab3f2a88e89e9c7c3a2259168b792d
Yasmina Bocereg <yasminabocereg@Yasminas-MacBook-Pro.local>
2022-04-28 13:01:20 +0300

    [6]:user-can-see-a-tetromino-fall-when-they-load-the-page

--- src/tetrominos.js
+++ src/tetrominos.js
@@ -1,4 +1,4 @@
-export const i = [[true], [true], [true], [true]];
+/*export const i = [[true], [true], [true], [true]];
 
 export const j = [
   [false, true],
@@ -29,4 +29,68 @@ export const z = [
 export const t = [
   [true, true, true],
   [false, true, false],
-];
+];*/
+
+export const TETROMINOS = {
+  L: {
+    shape: [
+      [true, false],
+      [true, false],
+      [true, true],
+    ],
+    color: "orange"
+  },
+  O: {
+    shape: [
+      [true, true],
+      [true, true],
+    ],
+    color: "yellow"
+  },
+  I: {
+    shape: [
+      [true],
+      [true],
+      [true],
+      [true],
+    ],
+    color: "blue"
+  },
+  J: {
+    shape: [
+      [false, true],
+      [false, true],
+      [false, true],
+      [false, true],
+    ],
+    color: "pink"
+  },
+  S: {
+    shape : [
+      [false, true, true],
+      [true, true, false],
+    ],
+    color: "red"
+  },
+  T: {
+    shape : [
+      [true, true, true],
+      [false, true, false],
+    ],
+    color: "purple"
+
+  },
+  Z: {
+    shape: [
+      [true, true, false],
+      [false, true, true],
+    ],
+    color: "green"
+  }
+};
+
+export const randomTetromino = () => {
+  const tetrominos = 'IJLOSTZ';
+  const randTetromino = tetrominos[Math.floor(Math.random() * tetrominos.length)];
+  return TETROMINOS[randTetromino];
+};
