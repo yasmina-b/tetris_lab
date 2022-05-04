03484e2bea13628b1b1f8162af3e0b9550ebc7a3
Yasmina Bocereg <yasminabocereg@Yasminas-MacBook-Pro.local>
2022-05-04 15:17:49 +0300

    [7]:user-gets-a-new-tetromino-at-the-top

--- src/hooks/useBoard.js
+++ src/hooks/useBoard.js
@@ -1,6 +1,11 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 import { randomTetromino } from "../tetrominos";
 import { getEmptyBoard } from "../utils/utils";
+
+const DIRECTION = {
+    up: 'up',
+    down: 'down'
+}
 
 export const useBoard = () => {
     const [board, setBoard] = useState(getEmptyBoard());
@@ -8,45 +13,87 @@ export const useBoard = () => {
     const player = useRef( {
         currentPos : { row: 0, column: 5 },
         tetromino : randomTetromino()
-    }) ;
+    });
 
 useEffect(() => {
-    updateBoard(board);
+   //updateBoard(board);
 },[])
 
-const updatePosition = () => {
+const updatePosition = useCallback ((direction = DIRECTION.down) => {
+    let verticalAdjustment = 0;
+    if(direction === DIRECTION.down) {
+        verticalAdjustment = 1;
+    } else if (direction === DIRECTION.up) {
+        verticalAdjustment = -1;
+    }
     player.current = {
-        currentPos: { row: player.current.currentPos.row + 1, column: player.current.currentPos.column },
-        tetromino: player.current.tetromino
-    };
-    updateBoard(board);
-};
+        currentPos: { 
+            row: player.current.currentPos.row + verticalAdjustment,
+            column: player.current.currentPos.column 
+        },
+            tetromino: player.current.tetromino
+    }
+
+},[]);
 
 const updateBoard = () => {
+    // 1. sterg piesa (pozitia veche)
     player.current.tetromino.shape.forEach((row,rowIdx) => {
         row.forEach((val, colIdx) => {
             const row = player.current.currentPos.row + rowIdx
             const column = player.current.currentPos.column + colIdx
+            if(val === true){
+                board[row][column] = null;
+            }
+           
+        });
+    });
+    // 2. muta piesa mai jos
 
-            if(row > 0){
-                board[row-1][column] = null;
+    updatePosition(DIRECTION.down);
+
+    // 3. verifica daca sunt coliziuni
+
+    let isCollided = false;
+    player.current.tetromino.shape.forEach((row,rowIdx) => {
+        row.forEach((val, colIdx) => {
+            if(val === true) {
+                const row = player.current.currentPos.row + rowIdx
+                const column = player.current.currentPos.column + colIdx
+                if(row > 19 || row < 0 || column < 0 || column > 11 || board[row][column] != null) {
+                    isCollided = true;
+                }
             }
         });
     });
 
-    player.current.tetromino.shape.forEach((row,rowIdx) => {
+    // 4. daca sunt coliziuni intoarce piesa inapoi + log
+    if(isCollided) {
+        console.log("Collision!!");
+        updatePosition(DIRECTION.up);
+    }
+
+    // 5. desenarea piesei
+    player.current.tetromino.shape.forEach((row, rowIdx) => {
         row.forEach((val, colIdx) => {
             const row = player.current.currentPos.row + rowIdx
             const column = player.current.currentPos.column + colIdx
 
             if(val === true) {
-                board[row][column] = player.current.tetromino.color
+                board[row][column] = player.current.tetromino.color;
             }
         });
     });
-    setBoard([...board]);
-};
 
-return [player.current, updatePosition, board];
+    if(isCollided) {
+        player.current = {
+            currentPos : {row: 0, column: 5},
+            tetromino : randomTetromino(),
+        }
+    }
+    setBoard([...board]);
+}
+
+    return [updateBoard, board];
 }
 
